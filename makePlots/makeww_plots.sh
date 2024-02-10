708d6f93ef1eed0124f9c45e2e32b88ef8184ae1
ceballos <ceballos@cern.ch>
2024-02-10 04:06:19 +0100

    new

--- makePlots/makeww_plots.sh
+++ makePlots/makeww_plots.sh
@@ -1,29 +1,29 @@
-#root -l -b -q finalPlotWWUnfolding.C+'("MLL"    , false)';
-#root -l -b -q finalPlotWWUnfolding.C+'("MLL0JET", false)';
-#root -l -b -q finalPlotWWUnfolding.C+'("MLL",     true)';
-#root -l -b -q finalPlotWWUnfolding.C+'("MLL0JET", true)';
+#root -l -b -q finalPlotWWUnfolding.C'("MLL"	, false)';
+#root -l -b -q finalPlotWWUnfolding.C'("MLL0JET", false)';
+#root -l -b -q finalPlotWWUnfolding.C'("MLL",	  true)';
+#root -l -b -q finalPlotWWUnfolding.C'("MLL0JET", true)';
 
-#root -l -b -q finalPlotWWUnfolding.C+'("DPHILL"    , false)';
-#root -l -b -q finalPlotWWUnfolding.C+'("DPHILL0JET", false)';
-#root -l -b -q finalPlotWWUnfolding.C+'("DPHILL",     true)';
-#root -l -b -q finalPlotWWUnfolding.C+'("DPHILL0JET", true)';
+#root -l -b -q finalPlotWWUnfolding.C'("DPHILL"    , false)';
+#root -l -b -q finalPlotWWUnfolding.C'("DPHILL0JET", false)';
+#root -l -b -q finalPlotWWUnfolding.C'("DPHILL",     true)';
+#root -l -b -q finalPlotWWUnfolding.C'("DPHILL0JET", true)';
 
-#root -l -b -q finalPlotWWUnfolding.C+'("PTL1"    , false)';
-#root -l -b -q finalPlotWWUnfolding.C+'("PTL10JET", false)';
-#root -l -b -q finalPlotWWUnfolding.C+'("PTL1",     true)';
-#root -l -b -q finalPlotWWUnfolding.C+'("PTL10JET", true)';
+#root -l -b -q finalPlotWWUnfolding.C'("PTL1"	 , false)';
+#root -l -b -q finalPlotWWUnfolding.C'("PTL10JET", false)';
+#root -l -b -q finalPlotWWUnfolding.C'("PTL1",     true)';
+#root -l -b -q finalPlotWWUnfolding.C'("PTL10JET", true)';
 
-#root -l -b -q finalPlotWWUnfolding.C+'("PTL2"    , false)';
-#root -l -b -q finalPlotWWUnfolding.C+'("PTL20JET", false)';
-#root -l -b -q finalPlotWWUnfolding.C+'("PTL2",     true)';
-#root -l -b -q finalPlotWWUnfolding.C+'("PTL20JET", true)';
+#root -l -b -q finalPlotWWUnfolding.C'("PTL2"	 , false)';
+#root -l -b -q finalPlotWWUnfolding.C'("PTL20JET", false)';
+#root -l -b -q finalPlotWWUnfolding.C'("PTL2",     true)';
+#root -l -b -q finalPlotWWUnfolding.C'("PTL20JET", true)';
 
-#root -l -b -q finalPlotWWUnfolding.C+'("PTLL"    , false)';
-#root -l -b -q finalPlotWWUnfolding.C+'("PTLL0JET", false)';
-#root -l -b -q finalPlotWWUnfolding.C+'("PTLL",     true)';
-#root -l -b -q finalPlotWWUnfolding.C+'("PTLL0JET", true)';
+#root -l -b -q finalPlotWWUnfolding.C'("PTLL"	 , false)';
+#root -l -b -q finalPlotWWUnfolding.C'("PTLL0JET", false)';
+#root -l -b -q finalPlotWWUnfolding.C'("PTLL",     true)';
+#root -l -b -q finalPlotWWUnfolding.C'("PTLL0JET", true)';
 
-#root -l -b -q finalPlotWWUnfolding.C+'("NJET",     false)';
-#root -l -b -q finalPlotWWUnfolding.C+'("NJET",     true)';
-#root -l -b -q finalPlotWWUnfolding.C+'("N0JET",    false)';
-root -l -b -q finalPlotWWUnfolding.C+'("NJETS",    true)';
+#root -l -b -q finalPlotWWUnfolding.C'("NJET",     false)';
+#root -l -b -q finalPlotWWUnfolding.C'("NJET",     true)';
+#root -l -b -q finalPlotWWUnfolding.C'("N0JET",    false)';
+root -l -b -q finalPlotWWUnfolding.C'("NJETS",    true)';
